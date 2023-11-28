99320863edcb560eee168838bf1ccf1590132ef9
Min kyung suk <smk93021@gmail.com>
2023-11-28 22:49:57 +0900

    jsptest

---
+++ src/test/java/com/green/jspboard/JspboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.jspboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
