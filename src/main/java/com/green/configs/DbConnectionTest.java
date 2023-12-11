50b38b08641a7f1c23808594bfdc1cddc1bb6c83
kimsukkyung0 <smk93021@gmail.com>
2023-12-11 15:46:09 +0900

    데이터베이스변경(default charset utf-8변경)

--- src/main/java/com/green/configs/DbConnectionTest.java
+++ src/main/java/com/green/configs/DbConnectionTest.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class DbConnectionTest {
     public static void main(String[] args) throws Exception{
-        String DB_URL ="jdbc:mysql://@localhost:3306/jspboard";
+        String DB_URL ="jdbc:mysql://@localhost:3306/jspboard2";
         String DB_USER = "root";
         String DB_PASSWORD = "green502";
 
--- src/main/webapp/WEB-INF/views/board/index3.jsp
+++ src/main/webapp/WEB-INF/views/board/index3.jsp
@@ -6,7 +6,7 @@
 <html lang="ko">
 <head>
     <meta charset="utf-8" http-equiv="Content-Type" content="text/html;">
-    <meta name="viewport" content="width=device-width", initial-scale="1">
+    <meta name="viewport" content="width=device-width">
     <title>jspboard</title>
 
 
